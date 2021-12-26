3f7229fdd6d69dbc75b6e24ab4500b46b8d687d4
Emerson <emerapmelo@gmail.com>
2021-12-26 17:38:24 -0300

    Requisito 6 realizado

--- script.js
+++ script.js
@@ -11,3 +11,39 @@ const imageContainer = document.querySelector('#meme-image');
 imageSelected.addEventListener('change', (event) => {
   imageContainer.src = event.value;
 });
+
+const container = document.querySelector('#meme-image-container');
+
+const button1 = document.createElement('button');
+container.appendChild(button1);
+button1.id = 'fire';
+button1.innerHTML = 'Fire';
+
+const button2 = document.createElement('button');
+container.appendChild(button2);
+button2.id = 'water';
+button2.innerHTML = 'Water';
+
+const button3 = document.createElement('button');
+container.appendChild(button3);
+button3.id = 'earth';
+button3.innerHTML = 'Earth';
+
+function bordaFire() {
+  const containerBordaFire = container;
+  containerBordaFire.style.border = '3px dashed red';
+}
+
+function bordaWater() {
+  const containerBordaWater = container;
+  containerBordaWater.style.border = '5px double blue';
+}
+
+function bordaEarth() {
+  const containerBordaEarth = container;
+  containerBordaEarth.style.border = '6px groove green';
+}
+
+button1.addEventListener('click', bordaFire);
+button2.addEventListener('click', bordaWater);
+button3.addEventListener('click', bordaEarth);
