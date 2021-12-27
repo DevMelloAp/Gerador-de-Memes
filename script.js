ad885ed7e77c6ed6c947541c2b71d97f500a74f2
Emerson <emerapmelo@gmail.com>
2021-12-27 19:21:59 -0300

    Requisito 7 realizado

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Criando um input de texto a ser exibido no container:
+
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 
@@ -5,29 +7,33 @@ textInput.addEventListener('keyup', () => {
   memeText.innerText = textInput.value;
 });
 
+// Criando evento para carregamento da imagem do upload a ser exibida no container:
+
 const imageSelected = document.querySelector('#meme-insert');
 const imageContainer = document.querySelector('#meme-image');
 
-imageSelected.addEventListener('change', (event) => {
-  imageContainer.src = event.value;
+imageSelected.addEventListener('change', () => {
+  const containerImage = imageContainer;
+  containerImage.src = URL.createObjectURL(imageSelected.files[0]);
 });
 
+// Criando 3 botões/eventos para alteração da borda do container:
+
 const container = document.querySelector('#meme-image-container');
+const buttonFire = document.createElement('button');
+container.appendChild(buttonFire);
+buttonFire.id = 'fire';
+buttonFire.innerHTML = 'Fire';
 
-const button1 = document.createElement('button');
-container.appendChild(button1);
-button1.id = 'fire';
-button1.innerHTML = 'Fire';
+const buttonWater = document.createElement('button');
+container.appendChild(buttonWater);
+buttonWater.id = 'water';
+buttonWater.innerHTML = 'Water';
 
-const button2 = document.createElement('button');
-container.appendChild(button2);
-button2.id = 'water';
-button2.innerHTML = 'Water';
-
-const button3 = document.createElement('button');
-container.appendChild(button3);
-button3.id = 'earth';
-button3.innerHTML = 'Earth';
+const buttonEarth = document.createElement('button');
+container.appendChild(buttonEarth);
+buttonEarth.id = 'earth';
+buttonEarth.innerHTML = 'Earth';
 
 function bordaFire() {
   const containerBordaFire = container;
@@ -44,6 +50,32 @@ function bordaEarth() {
   containerBordaEarth.style.border = '6px groove green';
 }
 
-button1.addEventListener('click', bordaFire);
-button2.addEventListener('click', bordaWater);
-button3.addEventListener('click', bordaEarth);
+buttonFire.addEventListener('click', bordaFire);
+buttonWater.addEventListener('click', bordaWater);
+buttonEarth.addEventListener('click', bordaEarth);
+
+// Criando 4 mini imagens de memes a serem selecionadas para exibição no container:
+
+const imageMeme1 = document.getElementById('meme-1');
+imageMeme1.addEventListener('click', () => {
+  const imageContain = imageContainer;
+  imageContain.src = 'imgs/meme1.png';
+});
+
+const imageMeme2 = document.getElementById('meme-2');
+imageMeme2.addEventListener('click', () => {
+  const imageContain = imageContainer;
+  imageContain.src = 'imgs/meme2.png';
+});
+
+const imageMeme3 = document.getElementById('meme-3');
+imageMeme3.addEventListener('click', () => {
+  const imageContain = imageContainer;
+  imageContain.src = 'imgs/meme3.png';
+});
+
+const imageMeme4 = document.getElementById('meme-4');
+imageMeme4.addEventListener('click', () => {
+  const imageContain = imageContainer;
+  imageContain.src = 'imgs/meme4.png';
+});
